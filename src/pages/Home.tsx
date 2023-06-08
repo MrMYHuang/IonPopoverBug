1f107f7b6a6d8d4ace0fac132e3fb38dee0ca73e
Meng-Yuan Huang <myh@live.com>
2023-06-08 09:42:42 +0800

    feat: add IonPopover test

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,10 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonPopover, IonButton, IonLabel } from '@ionic/react';
+import { useState } from "react";
 import ExploreContainer from '../components/ExploreContainer';
 import './Home.css';
 
 const Home: React.FC = () => {
+  const [open, setOpen] = useState(false);
   return (
     <IonPage>
       <IonHeader>
@@ -16,6 +18,12 @@ const Home: React.FC = () => {
             <IonTitle size="large">Blank</IonTitle>
           </IonToolbar>
         </IonHeader>
+        <IonButton onClick={() => {
+          setOpen(true);
+        }}>ABC</IonButton>
+        <IonPopover isOpen={open} onDidDismiss={() => {setOpen(false)}}>
+          <IonLabel>CDE</IonLabel>
+        </IonPopover>
         <ExploreContainer />
       </IonContent>
     </IonPage>
